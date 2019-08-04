84f14b1b953702d573340831319710714c83582e
Mubanga <mubangeriseme@yahoo.com>
2019-08-04 19:26:43 +0200

    Magic 8 Ball Challenge Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(Magic8BallApp());
 
@@ -13,7 +14,37 @@ class Magic8BallApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.blue.shade900,
         ),
+        backgroundColor: Colors.blue,
+        body: EightBall(),
       ),
+    );
+  }
+}
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  int _current_ball_number = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.center,
+      //    crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+            child: FlatButton(
+          child: Image.asset("images/ball$_current_ball_number.png"),
+          onPressed: () {
+            setState(() {
+              _current_ball_number = Random().nextInt(5) + 1;
+            });
+          },
+        ))
+      ],
     );
   }
 }
